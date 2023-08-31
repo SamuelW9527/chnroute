25b86163b0bc082367042b2dd168261f4cf6c173
SamuelW9527 <5203050+SamuelW9527@users.noreply.github.com>
2023-08-31 18:55:40 +0800

    Update ros-dpbr.sh
    添加ipv6支持

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -17,6 +17,23 @@ wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
 #其他
 wget --no-check-certificate -c -O other.txt https://ispip.clang.cn/othernet_cidr.txt
 
+#电信ipv6
+wget --no-check-certificate -c -O ctv6.txt https://ispip.clang.cn/chinatelecom_ipv6.txt
+#联通ipv6
+wget --no-check-certificate -c -O cuv6.txt https://ispip.clang.cn/unicom_cnc_ipv6.txt
+#移动ipv6
+wget --no-check-certificate -c -O cmv6.txt https://ispip.clang.cn/cmcc_ipv6.txt
+#铁通ipv6
+wget --no-check-certificate -c -O crtcv6.txt https://ispip.clang.cn/crtc_ipv6.txt
+#教育网ipv6
+wget --no-check-certificate -c -O cernetv6.txt https://ispip.clang.cn/cernet_ipv6.txt
+#长城宽带/鹏博士ipv6
+wget --no-check-certificate -c -O gwbnv6.txt https://ispip.clang.cn/gwbn_ipv6.txt
+#其他ipv6
+wget --no-check-certificate -c -O otherv6.txt https://ispip.clang.cn/othernet_ipv6.txt
+
+
+######电信移动pbr
 {
 echo "/ip route rule"
 
@@ -49,7 +66,7 @@ for net in $(cat other.txt) ; do
 done
 } > ../ros-pbr-CT-CMCC.rsc
 
-
+######电信移动dpbr
 {
 echo "/ip firewall address-list"
 
@@ -81,6 +98,39 @@ for net in $(cat other.txt) ; do
   echo "add list=dpbr-CT address=$net"
 done
 } > ../ros-dpbr-CT-CMCC.rsc
+
+######电信移动dpbr-ipv6
+{
+echo "/ipv6 firewall address-list"
+
+for net in $(cat ctv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat cuv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat cmv6.txt) ; do
+  echo "add list=dpbr-CMCC address=$net"
+done
+
+for net in $(cat crtcv6.txt) ; do
+  echo "add list=dpbr-CMCC address=$net"
+done
+
+for net in $(cat cernetv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat gwbnv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat otherv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+} > ../ros-dpbr-CT-CMCC-v6.rsc
 
 
 ######电信联通
@@ -115,6 +165,40 @@ for net in $(cat other.txt) ; do
   echo "add list=dpbr-CT address=$net"
 done
 } > ../ros-dpbr-CT-CU.rsc
+
+
+######电信联通v6
+{
+echo "/ipv6 firewall address-list"
+
+for net in $(cat ctv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat cuv6.txt) ; do
+  echo "add list=dpbr-CU address=$net"
+done
+
+for net in $(cat cmv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat crtcv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat cernetv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat gwbnv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+
+for net in $(cat otherv6.txt) ; do
+  echo "add list=dpbr-CT address=$net"
+done
+} > ../ros-dpbr-CT-CU-v6.rsc
 
 
 cd ..
